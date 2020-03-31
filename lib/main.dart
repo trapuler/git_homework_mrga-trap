f3437e8086dd71a138bf3d57927bc647ea4c32bb
Dado <davorkrivi@gmail.com>
2020-03-31 22:33:57 +0200

    added leading icon

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        leading:Icon(Icons.grade),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
